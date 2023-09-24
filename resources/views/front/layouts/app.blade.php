ed0ce117a433d823d3f87fb3194de0adef13fc4b
Gokhan <dinatopya03@gmail.com>
2023-09-24 20:51:15 +0300

    top bar

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -18,16 +18,6 @@
 <div class="min-h-screen bg-gray-100">
     @include('front.layouts.navigation')
 
-    <!-- Page Heading -->
-    @if (isset($header))
-        <header class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
-            </div>
-        </header>
-    @endif
-
-
     @yield('content')
 
     <div class="footer py-10">
--- resources/views/front/layouts/navigation.blade.php
+++ resources/views/front/layouts/navigation.blade.php
@@ -1,3 +1,30 @@
+<div class="primary-bg">
+    <div class="container flex justify-between items-center text-white text-sm">
+        <div class="flex-1 flex justify-start items-center">
+            <ul class="flex flex-row items-center justify-center gap-7">
+                <li class="flex justify-center items-center gap-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 3.75v4.5m0-4.5h-4.5m4.5 0l-6 6m3 12c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 014.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 00-.38 1.21 12.035 12.035 0 007.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 011.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 01-2.25 2.25h-2.25z" />
+                    </svg>
+                    <a href="tel:+905555555555">+90 555 555 55 55</a>
+                </li>
+                <li class="flex justify-center items-center gap-1">
+                    <img src="{{ asset('assets/img/icons8-whatsapp-24.png') }}" class="w-4 h-4" />
+                    <a href="tel:+905555555555">+90 555 555 55 55</a>
+                </li>
+                <li>
+                    <a href="">Sipariş Takibi</a>
+                </li>
+            </ul>
+        </div>
+        <div class="flex justify-end items-center p-2 bg-primary text-white gap-2 flex-1">
+            <span>Dil : </span>
+            <a class="hover-primary border hover:border-gray-300 hover:bg-gray-300 cursor-pointer p-1 py-0.5" href="">TR</a>
+            <a class="hover-primary border hover:border-gray-300 hover:bg-gray-300 cursor-pointer p-1 py-0.5" href="">EN</a>
+            <a class="hover-primary border hover:border-gray-300 hover:bg-gray-300 cursor-pointer p-1 py-0.5" href="">RU</a>
+        </div>
+    </div>
+</div>
 <div class="shadow z-10">
     <div class="container flex justify-between items-center header">
         <div class="flex-1">
@@ -18,9 +45,8 @@
                 </li>
             </ul>
         </nav>
-        <div class="font-bold flex-1 flex justify-end items-center gap-5">
-            <a class="hover-primary" href="">Account</a>
-            <a class="hover-primary border hover:border-gray-300 hover:bg-gray-300 cursor-pointer p-2">TR</a>
+        <div class="font-bold flex-1 flex justify-end items-center ">
+            <a class="hover-primary mr-2 px-3" href="">Account</a >
         </div>
     </div>
 </div>
