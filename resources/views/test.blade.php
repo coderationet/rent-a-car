aa6a99bb30025f9e51e7b79ce1ccc3ec22297b31
Gökhan Çelebi <dinatopya03@gmail.com>
2024-01-17 16:27:49 +0300

    Test Merge 1

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -4,9 +4,15 @@
         <div class="row">
             <div class="col-md-12">
                 <div id="data-range-conteiner">
+                    <label for="date-range">Date Range:</label>
                     <input  id="date-range" name="daterange">
                 </div>
             </div>
         </div>
     </div>
+
+    <div class="test">
+        Test Merge 1
+    </div>
+
 @endsection
