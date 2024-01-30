abefbb7c3c2658a6500d118c2717f6f6c271a68d
Gökhan Çelebi <dinatopya03@gmail.com>
2024-01-30 12:05:40 +0300

    yedek

--- resources/views/admin/layouts/datatable-actions.blade.php
+++ resources/views/admin/layouts/datatable-actions.blade.php
@@ -1,12 +1,18 @@
 <td>
     @if(isset($editUrl))
-        <a href="{{ $editUrl }}" class="btn btn-xs btn-primary">Edit</a>
+        <a href="{{ $editUrl }}" class="btn btn-xs btn-primary">
+            <i class="fa fa-edit"></i>
+            Edit
+        </a>
     @endif
     @if(isset($deleteUrl))
         <form action="{{ $deleteUrl }}" method="POST" style="display: inline-block;">
             @csrf
             @method('DELETE')
-            <button type="submit" class="btn btn-xs btn-danger" onclick="return confirm('Are you sure you want to delete this item?')">Delete</button>
+            <button type="submit" class="btn btn-xs btn-danger" onclick="return confirm('Are you sure you want to delete this item?')">
+                <i class="fa fa-trash"></i>
+                Delete
+            </button>
         </form>
     @endif
 
